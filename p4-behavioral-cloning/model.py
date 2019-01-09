6bbe260183a823a81008461e9f95c7f03c5ede15
markmisener <markmisener@gmail.com>
2019-01-09 12:13:32 -0800

    add trained model and video

--- p4-behavioral-cloning/model.py
+++ p4-behavioral-cloning/model.py
@@ -1,5 +1,6 @@
 import cv2
 import glob
+import random
 import pandas as pd
 import numpy as np
 from keras.optimizers import Adam
@@ -9,67 +10,91 @@ from keras.layers import Conv2D, Cropping2D, Dense, Dropout, Flatten, Lambda
 from sklearn.model_selection import train_test_split
 import matplotlib.image as mpimg
 
-IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS = 160, 320, 3
+IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS = 160, 320, 3
 
 
-def flip(image, measurement):
-    return np.fliplr(image), -measurement
+def augment(image, measurement):
+    """ Randomly augments an image and it's corresponding steering angle.
+
+    Args:
+        image (np.array): array representation of an image
+        measurement (float): steering angle
+
+    Returns:
+        image (np.array): array representation of an image
+        measurement (float): steering angle
+    """
+    # randomly flip the image and measurement
+    if np.random.rand() < 0.5:
+        image = np.fliplr(image)
+        measurement = -measurement
+    # randomly adjust the brighness of the image
+    if np.random.rand() < 0.5:
+        delta_pct = random.uniform(0.4, 1.2)
+        hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+        hsv[:, :, 2] = hsv[:, :, 2] * delta_pct
+        image = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
+    return image, measurement
 
 
 def load_image(image_file):
-    """ Load RGB images from a file
+    """ Loads RGB images from a file
 
-    Args
+    Args:
         image_file (str): name of image file
 
+    Returns:
+        numpy array representation of the image
     """
+    image_file = image_file.strip()
     if image_file.split('/')[0] == 'IMG':
         image_file = '/opt/carnd_p3/data/{}'.format(image_file)
-    return mpimg.imread(image_file.strip())
+    return mpimg.imread(image_file)
 
 
 def load_data(test_size, additional_training_data=False):
-    """ Load the data from a directory and split it into
+    """ Loads the data from a directory and split it into
     training and testing datasets.
 
     Args:
         test_size (float): percentage of data to hold out for testing
     Returns:
-        List containing train-test split of inputs. (X_train, X_val,  y_train, y_val)
+        List containing train-test split of inputs. (X_train, X_val, y_train, y_val)
     """
     header = None
     names = ['center', 'left', 'right', 'steering', 'throttle', 'brake', 'speed']
     data_df = pd.read_csv('/opt/carnd_p3/data/driving_log.csv',
                           header=header, names=names)
+
     if additional_training_data:
         for filename in glob.glob('training_data/*.csv'):
             tmp_df = pd.read_csv(filename, header=header, names=names)
             data_df = pd.concat([data_df, tmp_df])
 
+    # ignore header rows of additional training data
     data_df = data_df[(data_df.center != 'center') |
                       (data_df.left != 'left') |
                       (data_df.right != 'right')]
+
     X = data_df[['center', 'left', 'right']].values
     y = data_df['steering'].values
 
     return train_test_split(X, y, test_size=test_size)
 
 
-def build_model(rows, columns, channels):
-    """ Create keras sequential model, derived from NVIDIA's architecture
-    (https://arxiv.org/pdf/1604.07316v1.pdf), with some minor modifications.
+def build_model():
+    """ Creates a keras sequential model, derived from NVIDIA's architecture
+    (https://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf),
+    with some minor modifications.
 
     Args:
-        rows (int): the number of rows in the input
-        columns (int): the number of columns in the input
-        channels (int): the number of channels in the input
-
+        None
     Returns:
         keras sequential model
     """
     model = Sequential()
-    model.add(Cropping2D(cropping=((65,20), (0,0)), input_shape=(rows, columns, channels)))
-    model.add(Lambda(lambda x: x/127.5 - 1.))
+    model.add(Cropping2D(cropping=((65,20), (0,0)), input_shape=(IMG_HEIGHT,IMG_WIDTH,IMG_CHANNELS)))
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5))
     model.add(Conv2D(24, (5, 5), strides=(2, 2), activation='relu'))
     model.add(Dropout(0.5))
     model.add(Conv2D(36, (5, 5), strides=(2, 2), activation='relu'))
@@ -88,9 +113,18 @@ def build_model(rows, columns, channels):
 
 
 def batch_generator(image_paths, steering_angles, batch_size):
+    """ Creates a generator pull pieces of the data and process them on the fly.
+
+    Args:
+        image_paths (np.array): array of paths to image files
+        steering_angle (np.array): array of steering angles
+        batch_size (int): number of items to process
+
+    Returns:
+        images (np.array): array of images
+        measurements (np.array): array of steering angles
     """
-    """
-    images = np.empty([batch_size, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS])
+    images = np.empty([batch_size, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS])
     measurements = np.empty(batch_size)
     while True:
         i = 0
@@ -99,8 +133,7 @@ def batch_generator(image_paths, steering_angles, batch_size):
             image = load_image(center)
             measurement = float(steering_angles[index])
 
-            if np.random.rand() < 0.5:
-                image, measurement = flip(image, measurement)
+            image, measurement = augment(image, measurement)
 
             images[i] = image
             measurements[i] = measurement
@@ -110,20 +143,22 @@ def batch_generator(image_paths, steering_angles, batch_size):
                 break
         yield images, measurements
 
+
 def main():
     """Entry point for training the model"""
 
     # Hyperparameters
     test_size = 0.20
-    batch_size = 128
+    batch_size = 256
     epochs = 5
     verbose = 1
     additional_training_data = True
 
     print('Loading data...')
     X_train, X_val, y_train, y_val = load_data(test_size, additional_training_data=additional_training_data)
-    print('Building sequential model...')
-    model = build_model(IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
+
+    print('Building model...')
+    model = build_model()
 
     checkpoint = ModelCheckpoint('model_checkpoints/model-{epoch:03d}.h5',
                                  monitor='val_loss',
@@ -135,9 +170,9 @@ def main():
     model.compile(loss='mean_squared_error', optimizer=Adam(lr=0.0001))
 
     print('Training model...')
-    history_object = model.fit_generator(batch_generator(X_train, y_train, batch_size),
+    history_object = model.fit_generator(batch_generator(X_train, y_train, batch_size, correction),
                                          steps_per_epoch=len(X_train)/batch_size,
-                                         validation_data=batch_generator(X_val, y_val, batch_size),
+                                         validation_data=batch_generator(X_val, y_val, batch_size, correction),
                                          validation_steps=len(X_val)/batch_size,
                                          callbacks=[checkpoint],
                                          epochs=epochs,
@@ -145,7 +180,8 @@ def main():
 
     print('Saving model...')
     model.save('model.h5')
-    print('Complete!')
+
+    print('Model saved, training complete!')
 
 if __name__ == '__main__':
     main()
