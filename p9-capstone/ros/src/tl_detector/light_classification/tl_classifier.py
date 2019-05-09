5fe1940d80e28c5aa3857888ed90dc12d193e419
Mark Misener <markmisener@gmail.com>
2019-05-09 08:57:51 -0700

    add tl classifier and model

--- p9-capstone/ros/src/tl_detector/light_classification/tl_classifier.py
+++ p9-capstone/ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,23 @@
 from styx_msgs.msg import TrafficLight
+import tensorflow as tf
+import os
+import numpy as np
+import rospy
+import cv2
+from keras.models import load_model
+
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+        self.model = load_model(DIR_PATH + '/tl_detector_model.h5')
+        self.light_state = TrafficLight.UNKNOWN
+        self.classes = {
+            0: TrafficLight.RED,
+            1: TrafficLight.YELLOW,
+            2: TrafficLight.GREEN,
+            4: TrafficLight.UNKNOWN
+        }
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -15,5 +29,9 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
+        img_resized = cv2.resize(image, (80, 60))/255.
+        img = np.array([img_resized])
+        model_predict = self.model.predict(img)
+        if model_predict[0][np.argmax(model_predict[0])] > 0.5:
+            return self.classes[np.argmax(model_predict[0])]
         return TrafficLight.UNKNOWN
--- p9-capstone/ros/src/tl_detector/tl_detector.py
+++ p9-capstone/ros/src/tl_detector/tl_detector.py
@@ -90,17 +90,19 @@ class TLDetector(object):
             self.upcoming_red_light_pub.publish(Int32(self.last_wp))
         self.state_count += 1
 
-    def get_closest_waypoint(self, pose):
+    def get_closest_waypoint(self, x, y):
         """Identifies the closest path waypoint to the given position
             https://en.wikipedia.org/wiki/Closest_pair_of_points_problem
         Args:
-            pose (Pose): position to match a waypoint to
+            x (float): x position
+            y (float): y position
 
         Returns:
             int: index of the closest waypoint in self.waypoints
 
         """
-        return self.waypoint_tree.query([x, y], 1)[1]
+        closest_idx = self.waypoint_tree.query([x, y], 1)[1]
+        return closest_idx
 
     def get_light_state(self, light):
         """Determines the current color of the traffic light
@@ -131,7 +133,7 @@ class TLDetector(object):
 
         """
         closest_light = None
-        line_wp_idx = NOne
+        line_wp_idx = None
 
         # List of positions that correspond to the line to stop in front of for a given intersection
         stop_line_positions = self.config['stop_line_positions']
@@ -144,14 +146,14 @@ class TLDetector(object):
                 line = stop_line_positions[i]
                 temp_wp_idx = self.get_closest_waypoint(line[0], line[1])
                 d = temp_wp_idx - car_wp_idx
-                if d >= 0 and d < diff:
+                if 0 <= d < diff:
                     diff = d
                     closest_light = light
                     line_wp_idx = temp_wp_idx
 
         if closest_light:
             state = self.get_light_state(closest_light)
-            return light_wp_idx, state
+            return line_wp_idx, state
 
         return -1, TrafficLight.UNKNOWN
 
